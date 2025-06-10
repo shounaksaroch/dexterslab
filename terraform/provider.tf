0e83cda0930e73122a3341702bf9580cf257f4be
ShackledMystic <shounak.saroch@ingka.ikea.com>
2025-06-10 12:01:53 +0200

    SHSAR2-DEXTERSLAB -- Init in terraform directory

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,3 +18,13 @@ provider "azuread" {
   client_id = "b45ca867-8cf6-4fe3-9304-9a1f7c8ac234"
   use_oidc  = true
 }
+
+backend "azurerm" {
+    use_oidc             = true
+    use_azuread_auth     = true
+    tenant_id            = "720b637a-655a-40cf-816a-f22f40755c2c"
+    client_id            = "a7c35d02-5523-480f-8e2a-05116a52e0b9"
+    storage_account_name = "tfstate076"
+    container_name       = "tfstate"
+    key                  = "sashakt.terraform.tfstate"
+}
