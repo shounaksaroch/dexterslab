d78015cca473100fd5b9ec668c77f7df8f989265
ShackledMystic <shounak.saroch@ingka.ikea.com>
2025-06-11 08:01:15 +0200

    SHSAR2-DEXTERSLAB -- AZ LOGIN then INIT

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,13 +11,13 @@ terraform {
     }
   }
   backend "azurerm" {
-    use_oidc             = true
-    use_azuread_auth     = true
-    tenant_id            = "720b637a-655a-40cf-816a-f22f40755c2c"
-    client_id            = "a7c35d02-5523-480f-8e2a-05116a52e0b9"
-    storage_account_name = "tfstate076"
-    container_name       = "tfstate"
-    key                  = "sashakt.terraform.tfstate"
+    #use_oidc             = true
+    #use_azuread_auth     = true
+    #tenant_id            = "720b637a-655a-40cf-816a-f22f40755c2c"
+    #client_id            = "a7c35d02-5523-480f-8e2a-05116a52e0b9"
+    #storage_account_name = "tfstate076"
+    #container_name       = "tfstate"
+    #key                  = "sashakt.terraform.tfstate"
   }
 }
 
@@ -25,6 +25,6 @@ terraform {
 provider "azuread" {
   tenant_id = "570c8a00-bc2e-466a-918b-a6e4e6326c02"
   client_id = "b45ca867-8cf6-4fe3-9304-9a1f7c8ac234"
-  use_oidc  = true
+  #use_oidc  = true
 }
 
